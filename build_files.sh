0cda10dec404348378ba316abea8bf11b9673d91
Raunak <raunak.piro@gmail.com>
2023-01-24 15:50:40 +0530

    go

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python 3.10.9 -m pip install -r requirements.txt
-python 3.10.9 manage.py collectstatic --noinput --clear
+Python 3.10.9 -m pip install -r requirements.txt
+Python 3.10.9 manage.py collectstatic --noinput --clear
 echo " BUILD END"
